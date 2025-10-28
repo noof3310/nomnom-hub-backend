b110183c7b7484a692069c0e8f9ff4d82fa1211b
Noppanara Sakulsaowapakkul <noppanara.sakulsaowapakkul@priorsolution.co.th>
2025-10-29 00:26:23 +0700

    add logging

---
+++ internal/log/logger.go
@@ -0,0 +1,21 @@
+package log
+
+import (
+	"strings"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+func New(env string) *zap.Logger {
+	env = strings.ToLower(env)
+	cfg := zap.NewProductionConfig()
+
+	if env == "" || env == "development" || env == "dev" {
+		cfg = zap.NewDevelopmentConfig()
+	}
+
+	cfg.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	l, _ := cfg.Build()
+	return l
+}
